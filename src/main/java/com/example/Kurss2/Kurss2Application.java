92e050198b8eb4eb23c2b8cdef5d5fa1d6d704bb
User <git@github.com:gdsStern/HelloWorld.git>
2024-06-30 19:50:18 +0300

    Files

---
+++ src/main/java/com/example/Kurss2/Kurss2Application.java
@@ -0,0 +1,13 @@
+package com.example.Kurss2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurss2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kurss2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Kurss2/Kurss2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Kurss2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kurss2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
